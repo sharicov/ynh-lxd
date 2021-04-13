fbbc33d79c229195fbecb64da8659f1c009994a2
sharicov <sharicov@gmail.com>
2021-04-13 22:28:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
-#!/bin/bash
-#sources: 
-#https://linuxcontainers.org/lxc/getting-started/
-#https://techviewleo.com/run-linux-containers-with-lxc-lxd-on-ubuntu/
-#https://forum.yunohost.org/t/can-yunohost-just-be-run-in-lxd-containers/14576/3
-#https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-lxd-on-ubuntu-20-04
+sources: 
+https://linuxcontainers.org/lxc/getting-started/
+https://techviewleo.com/run-linux-containers-with-lxc-lxd-on-ubuntu/
+https://forum.yunohost.org/t/can-yunohost-just-be-run-in-lxd-containers/14576/3
+https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-lxd-on-ubuntu-20-04
 
-#example container named vas
+example container named vas
 
 apt install lxd #select latest
 sudo lxd init #all defaults besides size (16GB and accessibility over web: lxd server port 8443
 
-#Debian 10 (buster) container:
+Debian 10 (buster) container:
 
 lxc launch images:debian/buster vas
+
+apt install curl
 
 curl https://install.yunohost.org | bash
 
@@ -26,14 +27,12 @@ lxc list --columns ns4
 
 lxc exec vas bash
 
-apt install curl
-
 curl https://install.yunohost.org | bash
 
 Access global ip to proceed with yunohost installation
 
 Restore backup with certificate instead of post install
-yunohost backup restore ....tar #only certificate, app restore not working
+yunohost backup restore ....tar
 
 talescale vpn install:
 
@@ -42,6 +41,9 @@ curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.gpg | sudo apt-key 
 curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 
 sudo tailscale up
+
+#or tailscale up --advertise-exit-node # to use as an exit node to internet from other pears of the private network
+
 source: https://tailscale.com/download/linux/debian-bullseye
 
 Check the page https://login.tailscale.com/admin/machines to view your VPN network.
@@ -53,12 +55,15 @@ smbclient -L 10.209.116.1
 In nextcloud settings add smb external storages with proper sharename and samba username/pwd on the host
 
 Optional:
+
 lxc stop vas
 lxc delete vas
 lxc list --columns ns4 #show internal ip
 On HOST machine
-sudo mount /dev/sda2 on /media/usb1
-sudo mount /dev/sdb1 on /media/usb2
+mount /dev/sda2 /media/usb1
+mount /dev/sdb1 /media/usb2
+# not needed, just in case# systemctl restart smbd
+
 lxc launch ubuntu:20.04 vas
 
 lxc config device add vas vartest disk source=/mnt/ path=/mnt #passthrough /mnt from host (source) to container (path)
